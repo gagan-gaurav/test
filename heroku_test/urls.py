759e66c1fd0619349cd282530f4d2b515765b6cf
gagan-gaurav <iamgagangaurav@gmail.com>
2021-11-07 18:24:21 +0530

    a

--- heroku_test/urls.py
+++ heroku_test/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.views.static import serve
 from django.conf.urls import url
-from django.config import settings
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
